0b9c9952bc836b84df38665dada00c7cb6daa1a8
amitdevkar <amitdevkar1111@gmail.com>
2024-05-28 12:44:05 +0530

    my fifth commit

--- src/test/java/case2/A.java
+++ src/test/java/case2/A.java
@@ -4,6 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
     System.out.println("Hello this code is push to repositary");
+    System.out.println("Hello this code is push to repositary updated");
 	}
 
 }
---
+++ src/test/java/case2/B.java
@@ -0,0 +1,9 @@
+package case2;
+
+public class B {
+
+	public static void main(String[] args) {
+    System.out.println("Just i want to Update my code");
+	}
+
+}
